e1de205fcff495b74583d69a71cd61b16264f1bf
Bembinov <enfin@inbox.ru>
2020-03-08 23:14:28 +0300

    feat

--- src/dream-team.js
+++ src/dream-team.js
@@ -1,4 +1,10 @@
-module.exports = function createDreamTeam(/* members */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function createDreamTeam(members) {
+  if(!Array.isArray(members)) return false;
+  let team = [];
+  for(let member of members) {
+      if(typeof(member) === 'string') {
+          team.push(member.trim().toUpperCase()[0]);
+     }  }
+  return team.sort().join('');
 };
+  
