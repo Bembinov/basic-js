b3ba89a9c882be3b3704feb813c97ae29ed0201e
Bembinov <enfin@inbox.ru>
2020-03-08 23:03:24 +0300

    feat

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,6 +1,5 @@
 const MODERN_ACTIVITY= 15; 
 const HALF_LIFE_PERIOD= 5730;
-
 module.exports = function dateSample(sampleActivity) {
   if(typeof sampleActivity !== 'string') return false;
   let sampleActivityNum = parseFloat(sampleActivity);
